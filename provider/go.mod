eefc3b32ba8749b5c181f9e06193bc39d881cbac
Florian Schwab <me@ydkn.io>
2022-03-01 09:07:13 +0100

    get latet k0s version from github

--- provider/go.mod
+++ provider/go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/go-logr/logr v1.2.2 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
 	github.com/golang/glog v1.0.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/hashicorp/hcl/v2 v2.11.1 // indirect
 	github.com/imdario/mergo v0.3.12
 	github.com/k0sproject/dig v0.2.0
@@ -42,7 +43,7 @@ require (
 	golang.org/x/oauth2 v0.0.0-20220223155221-ee480838109b // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 	golang.org/x/time v0.0.0-20220224211638-0e9765cccd65 // indirect
-	google.golang.org/genproto v0.0.0-20220222213610-43724f9ea8cf // indirect
+	google.golang.org/genproto v0.0.0-20220228195345-15d65a4533f7 // indirect
 	google.golang.org/grpc v1.44.0
 	google.golang.org/protobuf v1.27.1
 	gopkg.in/yaml.v2 v2.4.0
--- provider/pkg/k0sctl/defaults.go
+++ provider/pkg/k0sctl/defaults.go
@@ -6,6 +6,7 @@ import (
 	"github.com/creasty/defaults"
 	"github.com/k0sproject/dig"
 	"github.com/k0sproject/k0sctl/cmd"
+	"github.com/k0sproject/k0sctl/integration/github"
 	"github.com/k0sproject/k0sctl/pkg/apis/k0sctl.k0sproject.io/v1beta1"
 	"github.com/k0sproject/k0sctl/pkg/apis/k0sctl.k0sproject.io/v1beta1/cluster"
 	"gopkg.in/yaml.v2"
@@ -14,13 +15,18 @@ import (
 // DefaultCluster returns a default cluster configuration.
 // see https://github.com/k0sproject/k0sctl/blob/main/cmd/init.go
 func DefaultClusterConfig() (*Cluster, error) {
+	k0sVersion, err := latestK0sVersion()
+	if err != nil {
+		return nil, err
+	}
+
 	cfg := v1beta1.Cluster{
 		APIVersion: v1beta1.APIVersion,
 		Kind:       "Cluster",
 		Metadata:   &v1beta1.ClusterMetadata{Name: "k0s-cluster"},
 		Spec: &cluster.Spec{
 			Hosts: make(cluster.Hosts, 0),
-			K0s:   &cluster.K0s{},
+			K0s:   &cluster.K0s{Version: k0sVersion},
 		},
 	}
 
@@ -45,4 +51,8 @@ func DefaultClusterConfig() (*Cluster, error) {
 	}
 
 	return &cluster, nil
+}
+
+func latestK0sVersion() (string, error) {
+	return github.LatestK0sVersion(false)
 }
